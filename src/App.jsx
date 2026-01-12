a88f919bd0d4d59ac0441cd0f0b6ab96c5f8d20a
jaameyaw <johnsonameyaw22@gmail.com>
2026-01-12 07:04:18 -0500

    Add HeroParagraph component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
-import Title from './components/Title'
+import Title from './components/Title';
+import HeroParagraph from './components/HeroParagrahp';
 function App() {
 
 
@@ -14,6 +15,7 @@ function App() {
             </div>
 
             <Title/>
+            <HeroParagraph/>
           </div>
 
         </div>
